b2ec570f96c585c8e921708f30abd7fc7225b4f0
kaiomagalhaes <contato@kaiomagalhaes.com.br>
2016-03-11 14:34:42 -0300

    creating project

---
+++ app/controllers/callback_controller.rb
@@ -0,0 +1,23 @@
+class CallbackController < ApplicationController
+  def index
+    r = fetch("https://ssocodelitt.azurewebsites.net/api/user?token=#{params[:token]}")
+    respond_to do |format|
+      format.json { render json:  r.body}
+    end
+  end
+
+  def fetch(uri, limit=10)
+    resource_uri = URI(uri)
+    http = Net::HTTP.new(resource_uri.hostname, resource_uri.port)
+    http.use_ssl = limit == 10
+    response = http.get(resource_uri, headers)
+    case response
+      when Net::HTTPSuccess then
+        response
+      when Net::HTTPRedirection then
+        fetch(response['location'], limit - 1)
+      else
+        response.error!
+    end
+  end
+end
